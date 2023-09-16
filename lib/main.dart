7c163aec8a8bc76c6c189f3aa469e06d3abd7b72
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-09-17 02:22:07 +0900

    riverpod置き換え準備

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:qiita_app/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
