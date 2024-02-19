69afe4063a42bef211aa323d6617c96471e2ace8
pg <h.tanaka.22.7.575@4nonome.com>
2024-02-19 10:02:23 +0900

    state整形

--- lib/entity/user/user.dart
+++ lib/entity/user/user.dart
@@ -6,12 +6,20 @@ part 'user.freezed.dart';
 @freezed
 class User with _$User {
   const factory User({
-    @JsonKey(name: 'id') required String id,
-    @Default('') @JsonKey(name: 'name') String name,
-    @Default('') @JsonKey(name: 'description') String description,
-    @JsonKey(name: 'followees_count') required int followeesCount,
-    @JsonKey(name: 'followers_count') required int followersCount,
-    @JsonKey(name: 'profile_image_url') required String profileImageUrl,
+    @JsonKey(name: 'id') //
+    required String id,
+    @Default('') //
+    @JsonKey(name: 'name')
+    String name,
+    @Default('') //
+    @JsonKey(name: 'description')
+    String description,
+    @JsonKey(name: 'followees_count') //
+    required int followeesCount,
+    @JsonKey(name: 'followers_count') //
+    required int followersCount,
+    @JsonKey(name: 'profile_image_url') //
+    required String profileImageUrl,
   }) = _Create;
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
